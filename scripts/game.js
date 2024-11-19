7733139567d7b9c0453a97105810108f864a1a25
Matimane <1matias.sainio@gmail.com>
2024-11-19 23:38:21 +0200

    Hazardit takas

--- scripts/game.js
+++ scripts/game.js
@@ -1,4 +1,4 @@
-import Player from './player.js';
+import Player from "./player.js";
 
 // Game configuration
 const config = {
@@ -27,6 +27,7 @@ let wasd;
 let spaceBar;
 let walls;
 let platforms;
+let hazards;
 let doors = [];
 let openedDoors = Array(24).fill(false);
 
@@ -37,29 +38,37 @@ const playerStartY = 500;
 function preload() {
   this.load.image("background", "assets/2testbackground.png");
   this.load.image("player", "assets/elf1.png");
-  this.load.image("wall", "assets/ground1.png");
-  this.load.image("platform", "assets/ground2.png");
+  this.load.image("platform", "assets/ground1.png");
+  this.load.image("wall", "assets/wall.png");
+  this.load.image("hazard", "assets/bomb.png");
   this.load.image("door", "assets/castledoors.png");
 }
 
 function create() {
-  const bg = this.add.tileSprite(0, 0, config.width * 2, config.height, "background").setOrigin(0, 0);
+  const bg = this.add
+    .tileSprite(0, 0, config.width * 2, config.height, "background")
+    .setOrigin(0, 0);
   bg.setDisplaySize(config.width * 2, config.height);
 
   this.physics.world.setBounds(0, 0, config.width * 2, config.height);
 
   // Walls group
   walls = this.physics.add.staticGroup();
-  walls.create(200, 200, "platform").setScale(0.5, 10).refreshBody();
-  walls.create(100, 250, "platform").setScale(0.5, 10).refreshBody();
-  walls.create(750, 300, "platform").setScale(0.5, 10).refreshBody();
-  walls.create(950, 350, "platform").setScale(0.5, 10).refreshBody();
+  walls.create(250, 180, "wall").setScale(0.5, 0.6).refreshBody();
+  walls.create(50, 250, "wall").setScale(0.7, 0.6).refreshBody();
+  walls.create(750, 300, "wall").setScale(0.5, 0.5).refreshBody();
+  walls.create(1050, 350, "wall").setScale(0.5, 0.5).refreshBody();
 
   // Platforms group
   platforms = this.physics.add.staticGroup();
-  platforms.create(200, 505, "wall").setScale(1, 0.5).refreshBody();
-  platforms.create(400, 470, "wall").setScale(1, 1).refreshBody();
-  platforms.create(500, 600, "wall").setScale(1000, 1).refreshBody();
+  platforms.create(200, 505, "platform").setScale(1, 0.5).refreshBody();
+  platforms.create(400, 470, "platform").setScale(1, 1).refreshBody();
+  platforms.create(500, 600, "platform").setScale(1000, 1).refreshBody();
+
+  // Hazards group
+  hazards = this.physics.add.staticGroup();
+  hazards.create(400, 250, "hazard").setScale(0.5).refreshBody();
+
 
   // Adding multiple doors
   for (let i = 1; i <= 24; i++) {
@@ -77,11 +86,16 @@ function create() {
 
   // Camera setup
   this.cameras.main.startFollow(player);
-  this.cameras.main.setBounds(0, 0, config.width * 2, config.height);  // Set world bounds
+  this.cameras.main.setBounds(0, 0, config.width * 2, config.height); // Set world bounds
 
   // Colliders
   this.physics.add.collider(player, walls);
   this.physics.add.collider(player, platforms);
+
+  // Reset player on touching hazards
+  this.physics.add.collider(player, hazards, () => {
+    player.resetPosition(playerStartX, playerStartY);
+  });
 
   // Input configuration
   cursors = this.input.keyboard.createCursorKeys();
