b28e1a055ce02ed2d709975b2a9813500584cb0b
mari-sku <mari.skurnik@gmail.com>
2024-11-26 16:33:44 +0200

    dad

--- scripts/game.js
+++ scripts/game.js
@@ -265,7 +265,7 @@ class MainGameScene extends Phaser.Scene {
     // Camera setup
     this.cameras.main.startFollow(this.player);
     this.cameras.main.setBounds(0, 0, 8000, 8000);
-    this.cameras.main.setZoom(0.4); // Set the zoom level
+    this.cameras.main.setZoom(0.1); // Set the zoom level
 
     // Colliders for the player
     this.physics.add.collider(this.player, this.walls);
